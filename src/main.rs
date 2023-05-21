a4bf23a63fbc0544c7119a5dbf10a216220e2080
gph <1598542650@qq.com>
2023-05-21 15:53:37 +0800

    尝试进行复数运算

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 //name:gaopenghao
-fn main() {
-    let mut x = 5;
-    let _y = 1;
-    println!("The value of x is: {}", x);
-    x = x+6;
-    println!("The value of x is: {}", x);
-}
+use num::complex::Complex;
+
+ fn main() {
+   let a = Complex { re: 2.1, im: -1.2 };
+   let b = Complex::new(11.1, 22.2);
+   let result = a + b;
+
+   println!("{} + {}i", result.re, result.im)
+ }
+
 
