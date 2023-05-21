baf0f37606efb950a7de1a23154a919218a40e1e
gph <1598542650@qq.com>
2023-05-21 15:29:05 +0800

    change on main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 //name:gaopenghao
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    let _y = 1;
+    println!("The value of x is: {}", x);
+    x = x+6;
+    println!("The value of x is: {}", x);
 }
+
