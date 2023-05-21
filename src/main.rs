523047d22380a967818dd0720c55d4213b1c1bd0
gph <1598542650@qq.com>
2023-05-22 00:56:15 +0800

    5.22 -1st

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 //name:gaopenghao
 
 fn main() {
-    let s = String::from("hello");  // s 进入作用域
+    let mut s = String::from("hello");
+    let a = &s;
+    println!("{}", s);
+    println!("{}", a);
+    println!("{:p}", a);
 
-    takes_ownership(s);             // s 的值移动到函数里 ...
-                                    // ... 所以到这里不再有效
-    let x = 5;                      // x 进入作用域
+    change(&mut s);//可变引用
+    println!("{}", s);
+}
 
-    makes_copy(x);                  // x 应该移动函数里，
-                                    // 但 i32 是 Copy 的，所以在后面可继续使用 x
-
-} // 这里, x 先移出了作用域，然后是 s。但因为 s 的值已被移走，
-  // 所以不会有特殊操作
-
-fn takes_ownership(some_string: String) { // some_string 进入作用域
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
     println!("{}", some_string);
-} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+}
 
-fn makes_copy(some_integer: i32) { // some_integer 进入作用域
-    println!("{}", some_integer);
-} // 这里，some_integer 移出作用域。不会有特殊操作
 
