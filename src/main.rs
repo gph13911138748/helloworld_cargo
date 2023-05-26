d23499ba6d7dc55c5bb24cdd78ba37064b53afff
gph <1598542650@qq.com>
2023-05-27 00:47:00 +0800

    5/27/2023

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 //name:gaopenghao
 
-fn main() {
-    let s1 = String::from("hello");
-    let s2 = String::from("world!");
-    // 在下句中，s1的所有权被转移走了，因此后面不能再使用s1
-    let s3 = s1 + &s2;
-    assert_eq!(s3,"hello,world!");
-    // 下面的语句如果去掉注释，就会报错
-    // println!("{}",s1);
+
+fn main(){
+    let b = "hello".to_string();
+    a(b);
+    println!("{}",b);
+}
+fn a(q: String) {
+    println!("{}", q);
 }
 
 
